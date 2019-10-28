064f0e4c5d08135282e2a603c2f4e7befa272d20
yj <mingoxmango@naver.com>
2019-10-28 19:24:43 +0900

    윤정 1028 19:24 백업 생성, 다운로드까지 완성!

--- Dsup/src/main/java/com/dsup/dbmanagement/service/BackupService.java
+++ Dsup/src/main/java/com/dsup/dbmanagement/service/BackupService.java
@@ -7,4 +7,5 @@ import com.dsup.dbmanagement.BackupVO;
 public interface BackupService {
 	public void BackupCreate(BackupVO vo, String tablespaceName);
 	public List<BackupVO> getBackupList(String userId);
+	public void backupDelete(BackupVO vo);
 }
