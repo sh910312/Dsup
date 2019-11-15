04e1a9d384e920804828d149c5ce7c33dcce410f
kuje5 <kuje5@LAPTOP-IUQG60NP>
2019-11-15 12:29:06 +0900

    191115 재문 커밋

--- Dsup/src/main/java/com/dsup/common/LoginInterceptor.java
+++ Dsup/src/main/java/com/dsup/common/LoginInterceptor.java
@@ -19,12 +19,12 @@ public class LoginInterceptor extends HandlerInterceptorAdapter{
         HttpSession session = request.getSession();
         MemberVO loginVO = (MemberVO) session.getAttribute("member");
  
-/*
+
         if(loginVO == null){
             response.sendRedirect("login");
             return false;
         }
-*/
+
         
         return true;
     }
